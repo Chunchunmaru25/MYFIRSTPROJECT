c72896a7027239d9a840ec64bb3dc8c91d7a1158
Chunchunmaru25 <clarenceduerme40@gmail.com>
2025-06-21 13:25:53 +0800

    [plkkkl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+          Route::middleware('web')
+        ->group(base_path('routes/user.php'));
+
+                  Route::middleware('web')
+        ->group(base_path('routes/admin.php'));
+
+                  Route::middleware('web')
+        ->group(base_path('routes/prof.php'));
     }
 }
